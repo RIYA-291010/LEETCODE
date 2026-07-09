7c579b1e92e67c9c08a08eee849c471a609e19f4
RIYA <chaudharyriya514@gmail.com>
2026-07-09 06:07:18 +0000

    Sync LeetCode submission Runtime - 3 ms (87.04%), Memory - 43.4 MB (41.31%)

---
+++ solutions/0020-valid-parentheses/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String str) {
+        Stack<Character> s=new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='('|| ch=='{'|| ch=='['){
+                s.push(ch);
+            }else{
+                if(s.isEmpty()){
+                  return false;
+                }
+                char top=s.peek();
+                if((top=='('&& ch==')')||( top=='{'&&ch=='}' )||(top=='[' && ch==']') ){
+                  s.pop();
+                }else{
+                    return false;
+                }
+            }
+            
+        }
+        return s.isEmpty();
+        
+    }
+}
